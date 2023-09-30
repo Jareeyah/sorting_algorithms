36a1dc134773614efc6ab9e5d7ffe13ad625ad3a
Jareeyah <jariatdosunmu@gmail.com>
2023-09-30 02:39:44 +0000

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,46 @@
 #include "sort.h"
-
 /**
- * swap_node - This program swaps node in a list
- * @first: This is the fist node
- * @sec: This is the second node
- * Return: void
- */
-void swap_node(listint_t *first, listint_t *sec)
-{
-	if (first->prev)
-		first->prev->next = sec;
-	if (sec->next)
-		sec->next->prev = sec;
-	first->next = sec->next;
-	sec->prev = first->prev;
-	first->prev = sec;
-	sec->next = first;
-
-}
-
-/**
- * insertion_sort_list - A function that sorts a doubly linked list
+ * insertion_sort_list -  A function that sorts a doubly linked list
  * of integers in ascending order
  * @list: The list to be printed
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *s, *l;
+        listint_t *l;
 
-	if (!list || !*list || !(*list)->next)
-		return;
-	s = (*list)->next;
-	while (s)
-	{
-		l = s;
-		l = s->next;
-		while (l && l->prev)
-		{
-			if (l->prev->n > l->n)
-			{
-				swap_node(l->prev, l);
-				if (!l->prev)
-					*list = l;
-				print_list((const listint_t *)*list);
-			}
-			else
-        l= l->prev;
-		}
+	      if (list == NULL || (*list)->next == NULL)
+		            return;
+	      l = (*list)->next;
+	      while (l)
+	      {
+		            while ((l->prev) && (l->prev->n > l->n))
+		            {
+			                  l = swap(l, list);
+			                  print_list(*list);
+		            }
+		            l = l->next;
+	      }
+}
+/**
+ * swap - This program swaps node in a list
+ * @first: The first node
+ * @sec: the secobd node
+ * Return: Pooiter to the node
+ */
+listint_t *swap(listint_t *first, listint_t **sec)
+{
+        listint_t *b = first->prev, *c = first;
 
-	}
+	      b->next = c->next;
+	      if (c->next)
+		            c->next->prev = b;
+	      c->next = b;
+	      c->prev = b->prev;
+	      b->prev = c;
+	      if (c->prev)
+		            c->prev->next = c;
+	      else
+		            *sec = c;
+	      return (c);
 }
